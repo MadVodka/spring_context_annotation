e61a3b5929d4e272f3fbc27944deda10e3602e0c
Ivan_Vatlin <ivan_valtin@epam.com>
2019-10-24 15:40:36 +0400

    Initial commit

---
+++ shop/src/main/java/org/shop/ShopLauncher.java
@@ -0,0 +1,22 @@
+package org.shop;
+
+
+import org.shop.configuration.ApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * The ShopLauncher class.
+ */
+public class ShopLauncher {
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        //TODO: implement using Spring Framework ApplicationContext
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+    }
+}
