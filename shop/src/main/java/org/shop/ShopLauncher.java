2990947cfdf8ce8b3492673e495387c328d00530
Ivan_Vatlin <ivan_valtin@epam.com>
2019-10-25 12:37:07 +0400

    Checking initialization of data done

--- shop/src/main/java/org/shop/ShopLauncher.java
+++ shop/src/main/java/org/shop/ShopLauncher.java
@@ -1,9 +1,15 @@
 package org.shop;
 
 
+import org.shop.api.ProductService;
+import org.shop.api.ProposalService;
+import org.shop.api.SellerService;
+import org.shop.api.UserService;
 import org.shop.configuration.ApplicationConfiguration;
+import org.shop.data.Product;
+import org.shop.data.Proposal;
+import org.shop.data.Seller;
 import org.shop.data.User;
-import org.shop.repository.UserRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -22,8 +28,30 @@ public class ShopLauncher {
     public static void main(String[] args) {
         //TODO: implement using Spring Framework ApplicationContext
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
-        List<User> users = userRepository.getUsers();
-        System.out.println(users);
+
+        System.out.println("Products");
+        ProductService productService = applicationContext.getBean(ProductService.class);
+        List<Product> products = productService.getProducts();
+        products.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Sellers");
+        SellerService sellerService = applicationContext.getBean(SellerService.class);
+        List<Seller> sellers = sellerService.getSellers();
+        sellers.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Proposals");
+        ProposalService proposalService = applicationContext.getBean(ProposalService.class);
+        List<Proposal> proposalsBySellerId = proposalService.getProposalsBySellerId(1L);
+        proposalsBySellerId.addAll(proposalService.getProposalsBySellerId(2L));
+        proposalsBySellerId.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Users");
+        UserService userService = applicationContext.getBean(UserService.class);
+        List<User> users = userService.getUsers();
+        users.forEach(System.out::println);
+        System.out.println();
     }
 }
