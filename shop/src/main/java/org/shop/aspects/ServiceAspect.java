566012b4afd2d8c84cf5b413ecc2d79755e3b173
Ivan_Vatlin <ivan_valtin@epam.com>
2019-10-29 16:13:07 +0400

    Some improvements

--- shop/src/main/java/org/shop/aspects/ServiceAspect.java
+++ shop/src/main/java/org/shop/aspects/ServiceAspect.java
@@ -3,8 +3,6 @@ package org.shop.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Aspect
